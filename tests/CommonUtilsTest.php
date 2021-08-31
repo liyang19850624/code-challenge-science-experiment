298c657707ae0b6025db5054bd8518490b7a016d
liyang19850624 <84837443+liyang19850624@users.noreply.github.com>
2021-08-31 19:22:26 +1000

    Update CommonUtilsTest.php

--- tests/CommonUtilsTest.php
+++ tests/CommonUtilsTest.php
@@ -10,20 +10,17 @@ use PHPUnit\Framework\Assert;
 class CommonUtilsTest extends TestCase {
 
     public function testDate1() {
-        $dateDiffer = new CommonUtils();
-        $diffInDays = $dateDiffer->daysInDiff("02/06/1983", "22/06/1983");
+        $diffInDays = CommonUtils::daysInDiff("02/06/1983", "22/06/1983");
         $this->assertEquals(19, $diffInDays);
     }
 
     public function testDate2() {
-        $dateDiffer = new CommonUtils();
-        $diffInDays = $dateDiffer->daysInDiff("04/07/1984", "25/12/1984");
+        $diffInDays = CommonUtils::daysInDiff("04/07/1984", "25/12/1984");
         $this->assertEquals(173, $diffInDays);
     }
 
     public function testDate3() {
-        $dateDiffer = new CommonUtils();
-        $diffInDays = $dateDiffer->daysInDiff("03/01/1989", "03/08/1983");
+        $diffInDays = CommonUtils::daysInDiff("03/01/1989", "03/08/1983");
         $this->assertEquals(1979, $diffInDays);
     }
 
